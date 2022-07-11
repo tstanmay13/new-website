5e355268becda36cd053dd899fb7710d35e0858a
tstanmay13 <tstanmay13@gmail.com>
2022-07-11 14:53:22 -0500

    reference error

--- index.js
+++ index.js
@@ -2,5 +2,18 @@
 // service_e0cmeyn
 // pDlKgbHRQ6pawlQE9
 
+function contact(event){
+    event.preventDefault();
+    emaijs
+        .sendForm(
+            'service_e0cmeyn',
+            'template_6j174dj',
+            event.target,
+            'pDlKgbHRQ6pawlQE9'
+        ) .then(() => {
+            console.log('This worked');
+        })
+    
+}
 
 
