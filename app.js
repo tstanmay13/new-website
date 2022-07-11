6f83a196c75f4f0a4c35ec041c930157b5b0299e
tstanmay13 <tstanmay13@gmail.com>
2022-07-11 18:31:02 -0500

    added image and some animation

--- app.js
+++ app.js
@@ -73,3 +73,21 @@ function moveBackground(event){
     }
 
 }
+
+
+$(window).scroll(function() {
+   if(checkVisible($('section'))){
+    //    $("section").css("opacity", 1);
+   }
+});
+
+function checkVisible(elm, eval){
+    eval = eval || "visible";
+    var vpH = $(window).height(), // Viewport Height
+        st = $(window).scrollTop(), // Scroll Top
+        y = $(elm).offset().top,
+        elementHeight = $(elm).height();
+    
+    if (eval == "visible") return ((y < (vpH + st)) && (y > (st - elementHeight)));
+    if (eval == "above") return ((y < (vpH + st)));
+}
