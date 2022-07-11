ba6450d3c262faaa4576c8567bdeb0e78bb8e10d
tstanmay13 <tstanmay13@gmail.com>
2022-07-11 16:15:48 -0500

    shapes background done

--- index.js
+++ index.js
@@ -58,3 +58,18 @@ function toggleContrast(){
         document.body.classList.remove("dark-theme")
     }
 }
+
+
+const scaleFactor = 1/20;
+function moveBackground(event){
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+    
+    for (let i =0; i<shapes.length; ++i){
+        const isOdd = i % 2 !==0;
+        const boolInt = isOdd? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px`
+    }
+
+}
